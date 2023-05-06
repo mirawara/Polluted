576011e7b68d6f3ebe192f607e2f235886f96677
EdoardoLoni <e.loni1@studenti.unipi.it>
2023-05-06 09:45:41 +0200

    ClassificationActivity - layout

---
+++ Polluted/app/src/main/java/it/unipi/dii/msss/polluted/ClassificationActivity.kt
@@ -0,0 +1,11 @@
+package it.unipi.dii.msss.polluted
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClassificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_classification)
+    }
+}
