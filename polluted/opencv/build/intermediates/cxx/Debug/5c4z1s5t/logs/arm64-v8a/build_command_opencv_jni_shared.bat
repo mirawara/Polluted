1ea69a940dd2887388848e0abf5699d6f77e666a
Lorenzo Mirabella <81705342+mirawara@users.noreply.github.com>
2023-05-15 14:57:36 +0200

    ok

--- polluted/opencv/build/intermediates/cxx/Debug/5c4z1s5t/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Development\\Android\\android-sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\sangu\\AndroidStudioProjects\\PollutedLastVersion\\polluted\\opencv\\.cxx\\Debug\\5c4z1s5t\\arm64-v8a" ^
-  opencv_jni_shared
